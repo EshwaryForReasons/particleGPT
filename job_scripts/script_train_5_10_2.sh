d6f47e0cb3f66654fec292b814b5189c91ffc0c9
EshwaryForReasons <mishra.eshwary@gmail.com>
2025-08-04 23:31:23 -0700

    Removed job scripts.

--- job_scripts/script_train_5_10_2.sh
+++
@@ -1,2 +0,0 @@
-cd $SCRATCH/particleGPT
-torchrun --standalone --nproc_per_node=4 train.py config/model_5_10_2.json
--- job_scripts/script_train_6_1_1.sh
+++
@@ -1,3 +0,0 @@
-cd $SCRATCH/particleGPT
-nvidia-smi
-torchrun --standalone --nproc_per_node=4 train.py config/model_6_1_1.json
